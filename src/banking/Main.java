d17d87d72dc697a92e1dbdbf9260f23fe394b8b0
Admin <Admin@DESKTOP-ILORHMQ>
2026-04-11 16:37:17 +0200

    Main got fixed

--- src/banking/Main.java
+++ src/banking/Main.java
@@ -4,7 +4,7 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello NovaBank");
+        System.out.println("Hello NovaBank , Hema got here");
 
         SwingUtilities.invokeLater(() -> {
 
