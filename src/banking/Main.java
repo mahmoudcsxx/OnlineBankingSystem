50fc865e5260cb9f6fea27a6301da44c858e8e54
Mahmoud <mahmoudmoustafaxx7@gmail.com>
2026-04-11 04:49:58 +0200

    Hello NovaBank

--- src/banking/Main.java
+++ src/banking/Main.java
@@ -4,6 +4,7 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello NovaBank");
 
         SwingUtilities.invokeLater(() -> {
 
